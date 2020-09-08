7f57f72a6c3f07bb09768e1336aa3fc4e5fbb67d
Matt Debnar <matthew.debnar@gmail.com>
2020-09-08 19:01:02 -0400

    update markers to circles with color scheme

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,6 +1,9 @@
-function createMap(significantEarthquakes) {
+//set USGS GeoJSON Earthquake URL from https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+var quakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-    // Create the tile layer that will be the background of our map
+function createMap(quakeData) {
+
+    // Create the tile layer that will be the light background of our map
     var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
       attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
       maxZoom: 10,
@@ -8,21 +11,30 @@ function createMap(significantEarthquakes) {
       accessToken: API_KEY
     });
 
-  // Create a baseMaps object to hold the lightmap layer
+    // Create the tile layer that will be the satellite background of our map
+    var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 10,
+        id: "satellite-streets-v11",
+        accessToken: API_KEY
+        });
+
+  // Create a baseMaps object to hold the lightmap and satellitemap layers
   var baseMaps = {
-    "Light Map": lightmap
+    "Light Map": lightmap,
+    "Satellite Map": satellitemap
   };
 
-  // Create an overlayMaps object to hold the bikeStations layer
+  // Create an overlayMaps object to hold the quakeData layer
   var overlayMaps = {
-    "Significant Earthquakes": significantEarthquakes
+    "Significant Earthquakes": quakeData
   };
 
     // Create the map object with options
     var map = L.map("map", {
         center: [37.09, -95.71],
-        zoom: 2,
-        layers: [lightmap, significantEarthquakes]
+        zoom: 3,
+        layers: [lightmap, quakeData]
       });
     
       // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
@@ -33,19 +45,54 @@ function createMap(significantEarthquakes) {
 
     function createMarkers(response) {
 
-        // Pull the "stations" property off of response.data
+        // Pull the "features" property off of response
         var earthquakes = response.features;
       
-        // Initialize an array to hold bike markers
+        // Initialize an array to hold earthquake markers
         var earthquakeMarkers = [];
       
-        // Loop through the stations array
+        // Loop through the features array
         for (var index = 0; index < earthquakes.length; index++) {
           var earthquake = earthquakes[index];
+
+        // Set circle color based on magnitude
+         var quakeColor = "";
+
+         if (earthquake.properties.mag < 1) {
+         //green
+         quakeColor = "#008000";
+         }
+         else if (earthquake.properties.mag < 2) {
+         //green-yellow
+         quakeColor = "#adff2f";
+         }
+         else if (earthquake.properties.mag  < 3) {
+         //yellow
+         quakeColor = "#FFFF00";
+         }
+         else if (earthquake.properties.mag  < 4) {
+         //orange
+         quakeColor = "#FFA500";
+          }
+         else if (earthquake.properties.mag  < 5) {
+          //orange-red
+          quakeColor = "#FF4500";
+         }
+          else {
+         //red
+         quakeColor = "#FF0000";
+         };
       
-          // For each station, create a marker and bind a popup with the station's name
-          var earthquakeMarker = L.marker([earthquake.geometry.coordinates[1], earthquake.geometry.coordinates[0]])
-            .bindPopup("<h3>" + earthquake.properties.title + "<h3><h3>MMI: " + earthquake.properties.mmi + "</h3>");
+          // For each earthquake, create a circle and bind a popup with the earthquake's place and magnitued (mag)
+          var earthquakeMarker = L.circle([earthquake.geometry.coordinates[1], earthquake.geometry.coordinates[0]], {
+              fillOpacity: 0.8,
+              //set the color based on the above logic
+              color: quakeColor,
+              fillColor: quakeColor,
+              //set the radius to the magnitued times X for better display
+              radius: (earthquake.properties.mag * 40000)
+          })
+            .bindPopup("<h3>" + earthquake.properties.place + "<h3><h3>Magnitude: " + earthquake.properties.mag + "</h3>");
       
           // Add the marker to the bikeMarkers array
           earthquakeMarkers.push(earthquakeMarker);
@@ -56,4 +103,4 @@ function createMap(significantEarthquakes) {
       }
 
 // Perform an API call to the Citi Bike API to get station information. Call createMarkers when complete
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson", createMarkers);
+d3.json(quakeURL, createMarkers);
